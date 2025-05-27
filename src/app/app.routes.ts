80022e313b14014dbd70f9e7ac652fcc84ee7aca
Bryan Garcia <garciavaldezbryanuziel04@gmail.com>
2025-05-26 20:01:06 -0600

    diseño register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-
+import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './auth/login/login/login.component'; // 👈 Asegúrate de que esta ruta sea correcta
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
+     { path: 'register', component: RegisterComponent }
 ];
